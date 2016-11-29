00d1fe11883d7d47237552866ded0d5971e0bfc0
Lupeman <jaimegunther@Jaimes-MacBook-Air.local>
2016-11-29 12:43:36 +1100

    create HTML, CSS & JS files

---
+++ what-where.js
@@ -0,0 +1,43 @@
+var player1;
+var player2;
+var veggies;
+var fruit;
+var playArea;
+var score;
+var gameType;
+
+var fruits = {
+    apricot: yellow,
+    grapefruit: yellow,
+    lemon: yellow,
+    pineapple: yellow,
+    banana: yellow,
+    apple: red,
+    cherries: red,
+    strawberry: red,
+    watermelon: red,
+    raspberry: red,
+    avocado: green,
+    grapes: green,
+    lime: green,
+    kiwi: green,
+    apple: green
+};
+
+var veggies = {
+  corn:yellow,
+  potato:yellow,
+  parsnip:yellow,
+  squash:yellow,
+  capsicum:yellow,
+  beetroot:red,
+  tomato:red,
+  radish:red,
+  pepper:red,
+  onion:red,
+  broccoli:green,
+  lettuce:green,
+  peas:green,
+  asparagus:green,
+  cucumber:green
+}
